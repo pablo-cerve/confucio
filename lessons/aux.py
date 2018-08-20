a4b453cc5c26caa7725b86c7c98ae5b301926790
pablo-cerve <pablo@theappraisallane.com>
2018-08-20 00:28:29 -0300

    Added lesson6.py

--- lessons/aux.py
+++ lessons/aux.py
@@ -4,9 +4,13 @@ from lesson2 import LESSON2
 from lesson3 import LESSON3
 from lesson4 import LESSON4
 from lesson5 import LESSON5
-# from lesson6 import LESSON6
+from lesson6 import LESSON6
+# from lesson6 import LESSON7
+# from lesson6 import LESSON8
+# from lesson6 import LESSON10
+# from lesson6 import LESSON11
 
-LESSONS = [LESSON1, LESSON2, LESSON3, LESSON4, LESSON5]  # , LESSON6]
+LESSONS = [LESSON1, LESSON2, LESSON3, LESSON4, LESSON5, LESSON6]  # , LESSON7, LESSON8, LESSON9, LESSON10, LESSON11]
 
 
 def get_lesson(lesson_id):
---
+++ lessons/lesson6.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+LESSON6 = [
+    ["QÙ", "去", "ir"],
+    ["YÓUYǑNG", "游泳", "nadar"],
+    ["ZUÓTIĀN", "昨天", "ayer"],
+    ["JĪNGJÙ", "京剧", "Ópera de Pekín"],
+    ["ZĚNMEYÀNG", "怎么样", "¿Qué tal? / ¿Cómo?"],
+    ["YǑUYÌSI", "有意思", "interesante"],
+    ["JĪNTIĀN", "今天", "hoy"],
+    ["TIĀN", "天", "día / cielo"],
+    ["TIĀNQÌ", "天气", "tiempo (atmosférico)"],
+    ["TÀI", "太", "demasiado"],
+    ["SHÍHOU", "时候", "tiempo (momento)"],
+    ["XIÀNZÀI", "现在", "ahora"],
+    ["MÍNGTIĀN", "明天", "mañana"],
+    ["YǑU", "有", "tener"],
+    ["SHÍJIĀN", "事件", "tiempo"],
+    ["SHUŌ", "说", "hablar, decir"],
+    ["YĪ", "一", "uno"]
+    ["BIÀN", "遍", "número de veces (de una acción) / por todas partes"],
+    ["DǍQIÚ", "打球", "jugar a la pelota"],
+    ["DǍ", "打", "jugar"],
+    ["QIÚ", "球", "pelota"],
+    ["BÀOQIÀN", "抱歉", "lo siento / disculpar"],
+    ["KǑNGPÀ", "恐怕", "quizás / tal vez"],
+    ["XÍNG", "行", "estar bien"]
+]
