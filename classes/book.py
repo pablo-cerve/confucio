1039edd48072b11962d158527959eea2410abe00
pablo-cerve <pablo@theappraisallane.com>
2020-09-10 08:08:15 -0300

    Added extra lessons to generate.py

--- classes/book.py
+++ classes/book.py
@@ -4,15 +4,30 @@ sys.path.append('.')
 
 class Book:
     LAST_LESSON = 22
+    EXTRA_LESSONS = {
+        1: [11, 12],
+        2: [20]
+    }
+    EXTRA_STR = "sup"
 
     @staticmethod
-    def book_lesson_numbers(book_number):
+    def book_lesson_numbers(book_number, include_extra=True):
         if book_number == 1:
-            return range(1, 15)  # [1, 2, ... 14]
-        if book_number == 2:
-            # return range(15, 27)  # [15, ... 26]
-            return range(15, Book.LAST_LESSON + 1)
-        raise ValueError("Invalid book_number: {}.".format(book_number))
+            numbers = range(1, 15)  # [1, 2, ... 14]
+        elif book_number == 2:
+            numbers = range(15, Book.LAST_LESSON + 1) # [15, ... Book.LAST_LESSON]
+        else:
+            raise ValueError("Invalid book_number: {}.".format(book_number))
+        numbers = ["0" + str(number) if number < 10 else str(number) for number in numbers]
+        return numbers if not include_extra else Book.add_extra(book_number, numbers)
+
+    @staticmethod
+    def add_extra(book_number, numbers):
+        extra_lessons = [str(number) + Book.EXTRA_STR for number in Book.EXTRA_LESSONS[book_number]]
+        numbers += extra_lessons
+        numbers.sort()
+        print(numbers)
+        return numbers
 
     @staticmethod
     def all_lesson_numbers():
@@ -20,6 +35,9 @@ class Book:
 
     @staticmethod
     def book_number(lesson_number):
+        if isinstance(lesson_number, basestring):
+            lesson_number = lesson_number.replace(Book.EXTRA_STR, "")
+            lesson_number = int(lesson_number)
         if 1 <= lesson_number <= 14:
             return 1
         if 15 <= lesson_number <= 26:
--- generate.py
+++ generate.py
@@ -9,6 +9,7 @@ def print_lessons(filename, lesson_numbers, word_keys=None):
     path = Common.GENERATED_PATH + filename
     lessons = []
     for lesson_number in lesson_numbers:
+        lesson_number = int(lesson_number) if lesson_number[0] == "0" in lesson_number else lesson_number
         lesson = Lesson(lesson_number)
         lessons.append(lesson)
     Writer.write_lessons(path, lessons, word_keys)
