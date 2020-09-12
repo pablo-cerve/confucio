ce28e9c86ba3f6d34d25117bec1dc3717ab4a38a
pablo-cerve <pablo@theappraisallane.com>
2020-09-12 12:31:47 -0300

    Minor improvements to sup csvs

--- csv_utils/common.py
+++ csv_utils/common.py
@@ -6,6 +6,7 @@ class Common:
     LESSONS_PATH = "/Users/pablocerve/Documents/CHINO/repo/confucio/lessons/"
     GENERATED_PATH = LESSONS_PATH + "generated/"
     FIRST_ROW = ['HANZI', 'PINYIN', 'DEF1', 'TIPO1', 'DEF2', 'TIPO2', 'LECCION', 'NUM']
+    EXTRA_STR = "sup"
 
     @staticmethod
     def book_path(book_number):
@@ -13,10 +14,10 @@ class Common:
 
     @staticmethod
     def book_lesson_path(book_number, lesson_number, lesson_extra=False):
-        filename = "lesson" + str(lesson_number) + ("_extra" if lesson_extra else "") + ".csv"
+        filename = "lesson" + str(lesson_number) + (Common.EXTRA_STR if lesson_extra else "") + ".csv"
         return Common.book_path(book_number) + filename
 
     @staticmethod
     def lesson_path(lesson):
-        filename = "lesson" + str(lesson.lesson_number) + ("_extra" if lesson.extra else "") + ".csv"
+        filename = "lesson" + str(lesson.lesson_number) + (Common.EXTRA_STR if lesson.extra else "") + ".csv"
         return Common.book_path(lesson.book_number) + filename
